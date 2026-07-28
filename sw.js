0e69f93815c5255577b83f7030c01cdcf1fe1da9
ravichandrac <87170529+ravichandrac@users.noreply.github.com>
2026-07-28 23:55:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pocket-shell-v13";
+const CACHE = "pocket-shell-v14";
 const APP_ROOT = new URL("./", self.location).pathname;
 const SHELL = [APP_ROOT, `${APP_ROOT}manifest.webmanifest`, `${APP_ROOT}favicon.svg`];
 
