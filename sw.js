b202473667b88b4b559db546afe4f415f3c9318f
ravichandrac <87170529+ravichandrac@users.noreply.github.com>
2026-07-28 10:28:21 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "pocket-shell-v2";
+const APP_ROOT = new URL("./", self.location).pathname;
+const SHELL = [APP_ROOT, `${APP_ROOT}manifest.webmanifest`, `${APP_ROOT}favicon.svg`];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match(APP_ROOT))));
+});
